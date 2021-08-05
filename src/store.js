3807e6c22c85291e3a742e0012f3cc0dfc1521a4
nickubed <nhpquandt@gmail.com>
2021-08-05 15:26:49 -0700

    with middleware

--- src/store.js
+++ src/store.js
@@ -1,8 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit'
 import dataReducer from './features/dataSlice'
 
+const logger = store => next => async action => {
+    console.log('dispatching', store.getState())
+    if (typeof action === 'function') {
+        action(store.dispatch, store.getState)
+    }
+    next(action)
+}
+
 export const store = configureStore({
     reducer: {
         data: dataReducer
-    }
+    },
+    middleware: [logger]
 })
