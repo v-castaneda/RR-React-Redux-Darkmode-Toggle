eebfde1ef15f9d93f6cfde3ee447cce904f95007
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2022-09-28 20:30:52 -0400

    added store and features

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 import './App.css';
 import { useSelector, useDispatch } from 'react-redux'
+import { clearData, fetchData, inputId, incrementId, decrementId } from './features/dataSlice'
 
 function App() {
-  // your logic goes here!
+  const dispatch = useDispatch ()
+  const data = useSelector( (state) => state.data)
+
+  const renderImg = () => {
+    if (data.apiData) {
+      return <img style={{'width': '100vw'}} src={data.apiData.primaryImage} alt={data.apiData.title} />
+    } else {
+      return <p>image here</p>
+    }
+  }
 
   return (
     <div className="App">
       <div>
-        <button onClick={() => {}}>Trigger Thunk</button>
-        <button onClick={() => {}}>Clear</button>
-        <button onClick={() => {}}>Next</button>
-        <button onClick={() => {}}>Back</button>
+        <button onClick={() => dispatch(fetchData())}>Thunk!</button>
+        <button onClick={() => dispatch(clearData())}>Clear</button>
+        <button onClick={() => dispatch(incrementId())}>Next</button>
+        <button onClick={() => dispatch(decrementId())}>Back</button>
       </div>
-      <input onChange={(e) => { }} />
+      <input value = { data.objectId } onChange={(e) => {
+        dispatch(inputId(Number(e.target.value)))
+       }} />
       <div>
-        {/* Once you have plugged everything in, render the image here! */}
+        {data.objectId}
+        {renderImg()}
       </div>
     </div>
   );
